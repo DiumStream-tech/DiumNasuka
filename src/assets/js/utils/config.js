f58d6d74242b99993b388f9d035036cd754ab3f7
DiumStream <diumstream@gmail.com>
2024-11-21 07:37:07 +0100

    1.0.1
    Patch :
    
    Pas d'affichage des News

--- src/assets/js/utils/config.js
+++ src/assets/js/utils/config.js
@@ -8,20 +8,18 @@ const nodeFetch = require("node-fetch");
 const convert = require('xml-js');
 let url = pkg.user ? `${pkg.url}/${pkg.user}` : pkg.url
 
-let configPath = `${url}/launcher/config-launcher/config.json`;
+let config = `${url}/launcher/config-launcher/config.json`;
 
 class Config {
-    async GetConfig() {
-        try {
-            const response = await nodeFetch(configPath);
-            if (response.status === 200) {
-                return await response.json();
-            } else {
-                throw new Error('server not accessible');
-            }
-        } catch (error) {
-            throw error;
-        }
+    GetConfig() {
+        return new Promise((resolve, reject) => {
+            nodeFetch(config).then(async config => {
+                if (config.status === 200) return resolve(config.json());
+                else return reject({ error: { code: config.statusText, message: 'server not accessible' } });
+            }).catch(error => {
+                return reject({ error });
+            })
+        })
     }
 
     async getInstanceList() {
@@ -39,32 +37,29 @@ class Config {
     }
 
     async getNews() {
-        try {
-            const config = await this.GetConfig();
-            const rssUrl = config.rss;
-            
-            if (!rssUrl) {
-                throw new Error('RSS URL not found in config');
-            }
+        const rssUrl = 'https://diumnasuka-adventures.dium-corp.com/api/rss';
+        
+        return new Promise((resolve, reject) => {
+            nodeFetch(rssUrl).then(async config => {
+                if (config.status === 200) {
+                    let news = [];
+                    let response = await config.text()
+                    response = (JSON.parse(convert.xml2json(response, { compact: true })))?.rss?.channel?.item;
 
-            const response = await nodeFetch(rssUrl);
-            if (response.status === 200) {
-                const xmlText = await response.text();
-                const jsonData = JSON.parse(convert.xml2json(xmlText, { compact: true }));
-                const items = jsonData.rss.channel.item;
-
-                return Array.isArray(items) ? items.map(item => ({
-                    title: item.title._text,
-                    content: item['content:encoded']._text,
-                    author: item['dc:creator']._text,
-                    publish_date: item.pubDate._text
-                })) : [];
-            } else {
-                throw new Error('server not accessible');
-            }
-        } catch (error) {
-            throw error;
-        }
+                    if (!Array.isArray(response)) response = [response];
+                    for (let item of response) {
+                        news.push({
+                            title: item.title._text,
+                            content: item['content:encoded']._text,
+                            author: item['dc:creator']._text,
+                            publish_date: item.pubDate._text
+                        })
+                    }
+                    return resolve(news);
+                }
+                else return reject({ error: { code: config.statusText, message: 'server not accessible' } });
+            }).catch(error => reject({ error }))
+        })
     }
 }
 
